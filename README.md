e3bc225b36611ed83d4c9f2ca69be01f2d43c452
Tyler Schrock <tschrock123@gmail.com>
2020-01-12 11:06:14 -0500

    Update build scripts

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ Rock-Checkin-Chrome
 **Rock-Checkin-Chrome** is a [Chromebook](https://www.google.com/chromebook/) app for the [Rock RMS](https://www.rockrms.com/) Check-in system. It is based off of the official [Rock Check-in](https://apps.apple.com/us/app/rock-check-in/id879253336) iOS app and aims to maintain feature parity.
 
 While it should work on any chromebook device, this app was designed to work with the [Acer Chromebook Tab 10](https://www.acer.com/ac/en/US/content/series/acerchromebooktab10) - a cheaper and easier to manage alternative to the iPad with a very similar form factor (It even fits in our old iPad stands! ...with some minor modifications).
+
+## Building
+
+```sh
+git checkout https://github.com/NewPointe/Rock-Checkin-Chrome.git
+cd Rock-Checkin-Chrome
+npm i
+npm run build
+```
+
+Unpackaged build results will be in `./dist`. You can add this folder as an unpackaged app inside of Chrome for testing. A packaged .zip will be in `./build`. This can be uploaded to the Chrome Web Store for your organization.
--- gulpfile.ts
+++ gulpfile.ts
@@ -5,7 +5,7 @@
  */
 'use strict';
 
-import { FSWatcher } from 'fs';
+import fs, { FSWatcher } from 'fs';
 
 import gulp from 'gulp';
 import del from 'del';
@@ -21,20 +21,79 @@ import webpackConfig from './webpack.config';
 
 (gulp_sass as any).compiler = node_sass;
 
-const fsWatchers: { watcher: FSWatcher, callback: () => void }[] = [];
 
-process.on("SIGINT", function () {
-    fsWatchers.forEach(w => {
-        w.watcher.close();
-        w.callback();
-    });
-    process.exit();
+// ============================== //
+//             Utils              //
+// ============================== //
+
+/**
+ * Partial typedef for a package.json file
+ */
+interface Package {
+    name: string;
+    version: string;
+}
+
+/**
+ * Reads a json file from disk
+ * @param path The path to the JSON file
+ */
+async function readJson<T>(path: string): Promise<T> {
+    return JSON.parse(await fs.promises.readFile(path, "utf-8")) as T;
+}
+
+/** Array of functions to run on quit. */
+const exitHandlers = new Set<() => Promise<void> | void>();
+
+/**
+ * Registers a function to be run when the proccess is quit.
+ * Usefull for stopping and cleaning up watch tasks/servers.
+ * @param handler
+ */
+function onExit(handler: () => Promise<void> | void) {
+    exitHandlers.add(handler);
+}
+
+// On Ctrl-C cleanup and exit
+process.on('SIGINT', async () => {
+
+    // Give them 2 seconds before forcibly quiting
+    setTimeout(() => process.exit(130), 2000);
+
+    // Notify all handlers
+    for (const h of exitHandlers) await h();
+
 });
 
+/**
+ * Closes the given watcher when the proccess is quit
+ * @param watcher The watcher to close
+ */
+async function closeOnExit(watcher: FSWatcher) {
+    return new Promise((resolve) => onExit(() => resolve(watcher.close())));
+}
+
+async function waitOnStream(stream: NodeJS.ReadWriteStream) {
+    return new Promise((resolve, reject) => stream.on("end", resolve).on("error", reject));
+}
+
+
+// ============================== //
+//             Tasks              //
+// ============================== //
+
+/**
+ * Cleans the build folders
+ */
 export function clean() {
     return del("./dist");
 }
+clean.description = "Cleans the build folders";
 
+/**
+ * Compiles TypeScript files using webpack
+ * @param watch If webpack should watch the files
+ */
 function compile_typescript(watch = false) {
     return gulp.src([
         "./src/scripts/application.ts",
@@ -42,14 +101,22 @@ function compile_typescript(watch = false) {
         "./src/scripts/client-api-injector.ts",
         "./src/scripts/client-api.ts"
     ])
-    .pipe(named())
-    .pipe(webpackStream({ watch, ...webpackConfig }, webpack as any))
-    .pipe(gulp.dest('dist/scripts/'));
+        .pipe(named())
+        .pipe(webpackStream({ watch, ...webpackConfig }, webpack as any))
+        .pipe(gulp.dest('dist/scripts/'));
 }
+
+/**
+ * Compiles TypeScript files
+ */
 export function typescript() {
     return compile_typescript(false);
 }
+typescript.description = "Compiles TypeScript files";
 
+/**
+ * Compiles sass files
+ */
 export function sass() {
     return gulp.src("./src/styles/**/*.scss")
         .pipe(sourcemaps.init())
@@ -57,32 +124,67 @@ export function sass() {
         .pipe(sourcemaps.write("."))
         .pipe(gulp.dest("./dist/styles"));
 }
+sass.description = "Compiles sass files";
 
+/**
+ * Copies static files
+ */
 export function copy() {
     return gulp.src(["./public/**/*"])
         .pipe(gulp.dest("./dist"));
 }
+copy.description = "Copies static files";
 
-export function packageapp() {
-    return gulp.src("./dist/**/*")
-        .pipe(gulp_zip('archive.zip'))
-        .pipe(gulp.dest('./'));
+/**
+ * Packages built files into a .zip
+ */
+export async function packageapp() {
+    const packageJson = await readJson<Package>("./package.json");
+    const outName = `${packageJson.name}-v${packageJson.version}.zip`;
+    return waitOnStream(
+        gulp.src("./dist/**/*")
+            .pipe(gulp_zip(outName))
+            .pipe(gulp.dest('./build'))
+    );
 }
+packageapp.description = "Packages built files into a .zip";
 
+/**
+ * Watches TypeScript files
+ */
 export function watch_typescript() {
     return compile_typescript(true);
 }
+watch_typescript.description = "Watches TypeScript files";
 
-export function watch_sass(callback: () => void) {
-    fsWatchers.push({ watcher: gulp.watch("./src/styles/**/*.scss", sass), callback });
+/**
+ * Watches sass files
+ */
+export async function watch_sass() {
+    return closeOnExit(gulp.watch("./src/styles/**/*.scss", sass));
 }
+watch_sass.description = "Watches sass files";
 
-export function watch_copy(callback: () => void) {
-    fsWatchers.push({ watcher: gulp.watch("./public/**/*", copy), callback });
+/**
+ * Watches static files
+ */
+export function watch_copy() {
+    return closeOnExit(gulp.watch("./public/**/*", copy));
 }
+watch_copy.description = "Watches static files";
 
+/**
+ * Watches all files
+ */
 export const watch = gulp.parallel(watch_sass, watch_typescript, watch_copy);
+watch.description = "Watches all files";
 
+/**
+ * Builds all files
+ */
 export const build = gulp.parallel(sass, typescript);
+build.description = "Builds all files";
 
-export default gulp.series(clean, copy, build, packageapp);
+const defaultTask = gulp.series(clean, copy, build, packageapp);
+defaultTask.description = "Builds and packages the app";
+export default defaultTask;
